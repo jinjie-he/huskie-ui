43342062bfd0c4442ad5f357519ee2701462057b
Junjie JJ He <junjie.jj.he@cn.pwc.com>
2021-12-23 18:42:22 +0800

    feat code commit

--- docs/components/table.md
+++ docs/components/table.md
@@ -9,10 +9,24 @@ desc: 'desc'
 
 ```vue
 <template>
-   <huskie-table>
+   <huskie-table :request="getTableData">
 
    </huskie-table>
 </template>
+<script lang="ts" setup>
+const getTableData = () => {
+ return  new Promise(resolve => {
+       resolve({
+         data:[{
+             name:'junjie',
+             age:'24',
+             sex:'男'
+         }],
+         total:1
+       })
+   })
+}
+</script>
 ```
 
 :::
--- packages/table/src/Index.vue
+++ packages/table/src/Index.vue
@@ -1,12 +1,67 @@
 <template>
     <div class="huskie-table">
-          <el-table></el-table>
+        <el-table :data="_data.data.length > 0 ? _data.data : dataSource">
+            <el-table-column
+                v-for="(column, index) in columns"
+                :title="column.title"
+                :prop="column.dataIndex"
+                :key="index"
+            />
+        </el-table>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue'
-import {ElTable} from 'element-plus'
-
-defineProps({})
+import { defineProps, onMounted, PropType, reactive } from 'vue'
+import { ElTable, ElTableColumn } from 'element-plus'
+interface Column {
+    title?: string
+    request?: () => Promise
+    dataIndex: string
+    search?: boolean
+}
+const props = defineProps({
+    dataSource: {
+        type: Array,
+        default: () => []
+    },
+    request: {
+        type: Function,
+        default: () => {
+            return new Promise(resolve => {
+                resolve({
+                    data: [],
+                    total: 0
+                })
+            })
+        }
+    },
+    params: {
+        type: Object,
+        default: () => {
+            return {}
+        }
+    },
+    columns: {
+        type: Array as PropType<Array<Column>>,
+        default: () => {
+            return {}
+        }
+    }
+})
+const _data = reactive<{
+    data: Array<never>
+}>({ data: [] })
+onMounted(() => {
+    getRequestData()
+})
+const getRequestData = async () => {
+    try {
+        const { data, total } = await props.request()
+        console.log(data, total, _data)
+        _data.data = data
+    } catch (e) {
+        console.log(e)
+    }
+}
 </script>
